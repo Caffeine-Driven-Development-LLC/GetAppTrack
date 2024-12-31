fff3dd65fad38c4888f000858723a0dd5641b001
Ian McNaughton <IanMcNaugh@gmail.com>
2024-12-30 22:28:18 -0600

    better?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type {ReactElement, ReactNode} from 'react'
 import type {NextPage} from 'next'
 import type {AppProps} from 'next/app'
 import theme from "@/app/theme";
-import {CssBaseline, Stack} from "@mui/material";
+import {Box, CssBaseline, Stack} from "@mui/material";
 import {ThemeProvider} from "@mui/material/styles";
 import AppTitle from "@/app/components/app_title";
 import Footer from "@/app/components/footer";
@@ -19,13 +19,13 @@ export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <div style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+      <Box sx={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
         <AppTitle/>
         <Stack spacing={3} sx={{padding: 3, flex: 1, marginLeft: 10, marginRight: 10}}>
           <Component {...pageProps} />
         </Stack>
         <Footer/>
-      </div>
+      </Box>
     </ThemeProvider>
   )
 }
