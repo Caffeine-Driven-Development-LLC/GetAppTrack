ba50a8a34f55f302da586db83b6d1355ed04d739
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-01-08 21:07:19 -0600

    added a link to applicaion event docs from sankey graph docs

--- src/pages/documentation/settings/sankey-diagram.tsx
+++ src/pages/documentation/settings/sankey-diagram.tsx
@@ -1,4 +1,4 @@
-import {Paper, Stack, Typography} from "@mui/material";
+import { Link, Paper, Stack, Typography} from "@mui/material";
 import DocumentationLayout from "@/components/documentation-layout";
 import ExampleSankeyGraph from "@/components/example-sankey-graph";
 import { Edit } from '@mui/icons-material'
@@ -17,7 +17,7 @@ export default function SankeyDiagramDocumentationPage() {
           </Typography>
         </Paper>
         <Typography fontFamily="Roboto">
-          Sankey Diagrams show a visual representation of how many applications have progressed with a simple visual of bars and nodes. Each node represents an application event, and the size of the bar indicates the number of applications that have taken the path between two events. It can be overwhelming to have a node for each Application Event, so here you can pick what evens you want to track.
+          Sankey Diagrams show a visual representation of how many applications have progressed with a simple visual of bars and nodes. Each node represents an <Link href={"/documentation/settings/application-events"}>Application Event</Link>, and the size of the bar indicates the number of applications that have taken the path between two events. It can be overwhelming to have a node for each Application Event, so here you can pick what evens you want to track.
         </Typography>
         <Typography fontFamily="Roboto">
           To create a node, simply click the Add Node button. This will ask you what you would like to name the node and the color it should appear as in the graph. After the node is created, you can edit it by selecting the <Edit fontSize="small"/> icon next to it. Here you can chose Application Events to associate with this node, change its name/color, or delete it altogether.
