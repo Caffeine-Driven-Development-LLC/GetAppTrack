60a8776d55842840addbbc052e2bc263fcefb30a
Ian McNaughton <IanMcNaugh@gmail.com>
2025-01-13 17:26:17 -0600

    company list docs

--- src/pages/documentation/companies/index.tsx
+++ src/pages/documentation/companies/index.tsx
@@ -1,5 +1,5 @@
-import DocumentationLayout from "@/components/documentation-layout";
-import {Stack, Typography} from "@mui/material";
+import DocumentationLayout from '@/components/documentation-layout';
+import { Link, Stack, Typography } from '@mui/material';
 
 export default function CompaniesDocumentationIndex() {
   return (
@@ -8,7 +8,26 @@ export default function CompaniesDocumentationIndex() {
         <Typography variant="h2">
           Companies
         </Typography>
+        <Typography>
+          Companies are listed and managed from this page, basic information is displayed for each company including the
+          total applications sent and how long its been since the last application to the company.
+        </Typography>
+        <Typography>
+          Companies are sorted by alphabetical order, but the star in the upper right can be selected to favorite a
+          company to make sure it appears near the top of the list. Companies can also be searched for using the search
+          bar.
+        </Typography>
+        <Typography>
+          Selecting any company will take you to that <Link href={'/documentation/companies/company-details'}>companies
+          details</Link> page. where you can find
+          and edit all of the tracked information for a company, as well as a list of applications for the company.
+        </Typography>
+        <Typography>
+          The Add Company button will bring up a modal asking for more information about the company to be added. See
+          the <Link href={'/documentation/companies/add-company'}>Add Company</Link> page for more documentation on
+          this.
+        </Typography>
       </Stack>
     </DocumentationLayout>
-  )
+  );
 }
