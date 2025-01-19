d5b657c97ed352db7990d913ed13f85c899e9c8f
Ian McNaughton <IanMcNaugh@gmail.com>
2025-01-18 19:00:10 -0600

    rough draft of settings docs

--- src/pages/documentation/settings/index.tsx
+++ src/pages/documentation/settings/index.tsx
@@ -1,7 +1,7 @@
-import {Link, Stack, Typography} from "@mui/material";
-import {Settings} from "@mui/icons-material";
-import DocumentationLayout from "@/components/documentation-layout";
-import React from "react";
+import { Link, Stack, Typography } from '@mui/material';
+import { Settings } from '@mui/icons-material';
+import DocumentationLayout from '@/components/documentation-layout';
+import React from 'react';
 
 export default function SettingsDocumentationPage() {
   return (
@@ -11,54 +11,59 @@ export default function SettingsDocumentationPage() {
           Settings
         </Typography>
         <Typography>
-          The Settings page is accessible by clicking the <Settings color={"primary"}/> icon in the bottom left of the
-          application. Here you can configure App Track to your needs.
+          Access your Settings page by clicking the <Settings color={'primary'} /> icon in the bottom left of the
+          application. Here you can configure App Track from its original default settings to align more with your
+          specific needs.
         </Typography>
         <Typography variant="h4">
           General
         </Typography>
         <Typography>
-          Selecting automatically check for updates, will let the application periodically check if a newer version of
-          the App Track application is released. Turning this off presents a button you can use to manually check for
-          updates.
+          Selecting the "Automatically Check for Updates" switch will let App Track periodically check if a newer
+          version of App Track is available. If this switch is in the off position, you can use the button to manually
+          check for updates.
         </Typography>
         <Typography>
-          The appearance simply changes the theme of the application, and allows you to pick from light, dark, or system
-          default.
+          The appearance changes the theme of the application and allows you to choose from light, dark, or system
+          default modes.
         </Typography>
         <Typography variant="h4">
           Application Tracking
         </Typography>
         <Typography>
-          The &#34;ghost&#34; period is the number of days an application will be considered active if it is not
-          updated. After the allotted number of days with no updates, the application is considered &#34;ghosted&#34;,
-          and will no long show on the Applications page. Any ghosted application can still be accessed, either by
-          searching for it on the applications page or by navigating to the company and selecting it in the list of
-          applications presented there.
+          App Track monitors a "ghost" period, which is the number of days an application will be considered active
+          without being update. After the allotted number of days sans updates, the application is considered "ghosted"
+          (much like a bad date), and will no longer show on your Applications page.
         </Typography>
         <Typography>
-          Selecting the Configure Application Events button will allow you to change the events and order of events that
-          an application can use. See the <Link href={"/documentation/settings/application-events"}>Configure
-          Application Events</Link> section for more information around what configurations are possible for events.
+          You still have the ability to access ghosted applications, either by searching for it on the Applications page
+          or by navigating to the Company and selecting it from your list of applications there.
         </Typography>
         <Typography>
-          Selecting the Configure Sankey Diagram button will allow you customize the graphs in the company details page.
-          The <Link href={"/documentation/settings/sankey-diagram.tsx"}>Configure Sankey Diagram</Link> page has more
-          documentation around what is possible with this graph.
+          <Link
+            href={'/documentation/settings/application-events'}>Configure Application Events</Link> will allow you to
+          alter the types of events (or sequential
+          order of events) that are available to choose from.
+        </Typography>
+        <Typography>
+          <Link href={'/documentation/settings/sankey-diagram.tsx'}>Configure Sankey Diagram</Link> will allow you to
+          view a customizable visual representation of your applications.
         </Typography>
         <Typography variant="h4">
           Danger Zone
         </Typography>
         <Typography>
-          The danger zone is where you would delete data. Currently there are 2 options, delete all data, or delete
-          Application data. Deleting all data will remove all Application, Company, and Application Events data.
-          Deleting Application data will only remove applications, any Companies or application events have been added
-          or modified will be retained.
+          The "danger zone" is where you would delete data. Currently, there are two options: Delete All Data, or Delete
+          Application Data.
         </Typography>
         <Typography>
-          Keep in mind, both of these actions are irreversible. once data is deleted it cannot be recovered.
+          Deleting All Data will remove all Application, Company, and Application Events data.
+        </Typography>
+        <Typography>
+          Deleting Application Data will only remove applications; any Companes or Events will remain. Please note that
+          both of the above actions are permanent. Once you delete data, it cannot be recovered.
         </Typography>
       </Stack>
     </DocumentationLayout>
-  )
+  );
 }
