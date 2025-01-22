4395f58a656d3fc00858999d6a56fd77010df41b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-01-21 21:36:20 -0600

    setting docs page review

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,4 @@
-import {Box, Divider, Stack, Typography} from "@mui/material";
+import { Box, Divider, Stack, Typography } from '@mui/material';
 
 export default function Footer() {
   // TODO Add links to Github for code and email for the company
@@ -7,33 +7,41 @@ export default function Footer() {
       component="footer"
       sx={{
         padding: 3,
-        bgcolor: "primary.light"
+        bgcolor: 'primary.light',
       }}
     >
-      <Stack spacing={2} sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-        <Stack direction={{xs: 'column', sm: 'row'}}
-               divider={<Divider orientation="vertical" flexItem/>}
-               spacing={2}
+      <Stack
+        spacing={2}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Stack
+          direction={{ xs: 'column', sm: 'row' }}
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={2}
         >
           <Typography>Github</Typography>
-          <Typography>Contact Us</Typography>
+          <Typography>Support@getAppTrack.com</Typography>
         </Stack>
-        <Typography variant="body2" sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center"
-        }}>
+        <Typography
+          variant="body2"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}
+        >
           App Track is the result of continuous focus on self-improvement, both
-          professionally and personally. App Track was built in Chicago, Illinois. Do you know what else is located in
-          Chicago, Illinois? Coffee shops. Want me to be able to visit one of those coffee shops? Please click here to
-          buy me a coffee.
+          professionally and personally. App Track was built in Chicago,
+          Illinois. Do you know what else is located in Chicago, Illinois?
+          Coffee shops. Want me to be able to visit one of those coffee shops?
+          Please click here to buy me a coffee.
         </Typography>
       </Stack>
     </Box>
-  )
+  );
 }
--- src/pages/documentation/settings/index.tsx
+++ src/pages/documentation/settings/index.tsx
@@ -7,61 +7,69 @@ export default function SettingsDocumentationPage() {
   return (
     <DocumentationLayout>
       <Stack spacing={2}>
-        <Typography variant="h2">
-          Settings
+        <Typography variant="h2">Settings</Typography>
+        <Typography>
+          To access the Settings page, click the{' '}
+          <Settings color={'primary'} fontSize={'small'} /> ion in the bottom
+          left of the app. This page can be used to configure App Track from its
+          default settings to better align with your specific needs.
+        </Typography>
+        <Typography variant="h4">General</Typography>
+        <Typography>
+          App Track has the ability to let you know if App updates are
+          available. To enable this, select the &#34;Automatically Check for
+          Updates&#34;. If this switch is not enabled, you also have the ability
+          to use the button to manually check for updates.
         </Typography>
         <Typography>
-          Access your Settings page by clicking the <Settings color={'primary'} /> icon in the bottom left of the
-          application. Here you can configure App Track from its original default settings to align more with your
-          specific needs.
+          Appearance allows you to change the theme of the app. Options
+          available are the standard Light, Dark, or System Default modes.
         </Typography>
-        <Typography variant="h4">
-          General
+        <Typography variant="h4">Application Tracking</Typography>
+        <Typography>
+          Unlike online dating, App Track can actually can let you know when
+          you&#39;re being ghosted. The app monitors a &#34;ghost&#34; period,
+          which is the number of days that an application will still be
+          considered active without an update. After the allotted number of days
+          sans updates, the application will be considered &#34;ghosted&#34; and
+          will no longer show on your Applications page.
         </Typography>
         <Typography>
-          Selecting the "Automatically Check for Updates" switch will let App Track periodically check if a newer
-          version of App Track is available. If this switch is in the off position, you can use the button to manually
-          check for updates.
+          You will still have the ability to access ghosted applications, either
+          by searching for it on the Applications page or by navigating to the
+          Company and selecting it from the list of previous applications.
         </Typography>
         <Typography>
-          The appearance changes the theme of the application and allows you to choose from light, dark, or system
-          default modes.
-        </Typography>
-        <Typography variant="h4">
-          Application Tracking
-        </Typography>
-        <Typography>
-          App Track monitors a "ghost" period, which is the number of days an application will be considered active
-          without being update. After the allotted number of days sans updates, the application is considered "ghosted"
-          (much like a bad date), and will no longer show on your Applications page.
+          <Link href={'/documentation/settings/application-events'}>
+            Configure Application Events
+          </Link>{' '}
+          will allow you to alter the types of events (or sequential order of
+          events) that are available to choose from.
         </Typography>
         <Typography>
-          You still have the ability to access ghosted applications, either by searching for it on the Applications page
-          or by navigating to the Company and selecting it from your list of applications there.
+          <Link href={'/documentation/settings/sankey-diagram.tsx'}>
+            Configure Sankey Diagram
+          </Link>{' '}
+          will allow you to view a customizable visual representation of the
+          lifeline of your applications.
         </Typography>
+        <Typography variant="h4">Danger Zone</Typography>
         <Typography>
-          <Link
-            href={'/documentation/settings/application-events'}>Configure Application Events</Link> will allow you to
-          alter the types of events (or sequential
-          order of events) that are available to choose from.
-        </Typography>
-        <Typography>
-          <Link href={'/documentation/settings/sankey-diagram.tsx'}>Configure Sankey Diagram</Link> will allow you to
-          view a customizable visual representation of your applications.
-        </Typography>
-        <Typography variant="h4">
-          Danger Zone
-        </Typography>
-        <Typography>
-          The "danger zone" is where you would delete data. Currently, there are two options: Delete All Data, or Delete
+          The &#34;Danger Zone&#34; is where you can delete data. Currently,
+          there are two available options: Delete All Data, or Delete
           Application Data.
         </Typography>
         <Typography>
-          Deleting All Data will remove all Application, Company, and Application Events data.
+          Deleting All Data will remove all Application, Company, and
+          Application Events data.
         </Typography>
         <Typography>
-          Deleting Application Data will only remove applications; any Companes or Events will remain. Please note that
-          both of the above actions are permanent. Once you delete data, it cannot be recovered.
+          Deleting Application Data will only remove applications; any Companies
+          added or Events will remain.
+        </Typography>
+        <Typography>
+          Please Note: Both of the above actions with deleting data are
+          permanent. Once data is delivered, it cannot be recovered.
         </Typography>
       </Stack>
     </DocumentationLayout>
