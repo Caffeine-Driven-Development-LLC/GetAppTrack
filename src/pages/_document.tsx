17ac60c719d8f7781216be64157b40399c0d4afb
Ian McNaughton <IanMcNaugh@gmail.com>
2026-01-05 20:42:56 -0600

    added google analytics again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,36 @@
-import {Head, Html, Main, NextScript} from 'next/document'
-import {AppRouterCacheProvider} from "@mui/material-nextjs/v15-appRouter";
-import {Container} from "@mui/material";
+import { Head, Html, Main, NextScript } from 'next/document';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { Container } from '@mui/material';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/icon.ico"/>
+        <link rel="icon" href="/icon.ico" />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FJ7K9WMVT6"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FJ7K9WMVT6');
+            `,
+          }}
+        />
       </Head>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FJ7K9WMVT6"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-FJ7K9WMVT6');
-      </script>
       <body>
-      <Container disableGutters={true} maxWidth={'xl'}>
-        <AppRouterCacheProvider>
-          <Main/>
-          <NextScript/>
-        </AppRouterCacheProvider>
-      </Container>
+        <Container disableGutters={true} maxWidth={'xl'}>
+          <AppRouterCacheProvider>
+            <Main />
+            <NextScript />
+          </AppRouterCacheProvider>
+        </Container>
       </body>
     </Html>
-  )
+  );
 }
