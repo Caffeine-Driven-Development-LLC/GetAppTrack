bd59c5d1fceb0c31af4743821d8fb6d7935cb77f
Ian McNaughton <ianmcnaugh@gmail.com>
2026-01-05 20:33:59 -0600

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to the document.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/icon.ico"/>
       </Head>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FJ7K9WMVT6"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-FJ7K9WMVT6');
+      </script>
       <body>
       <Container disableGutters={true} maxWidth={'xl'}>
         <AppRouterCacheProvider>
